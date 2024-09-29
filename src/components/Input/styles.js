fdaa8a82e59b40cbd06eeffa7e8f6eec2df4c618
RhuamG <106784443+rhuamgo@users.noreply.github.com>
2024-09-29 20:11:24 -0300

    Add files via upload

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+//yarn add styled-components instla o pacote
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+ 
+    
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        display:flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        
+        font-size: 24px;
+        font-family: "Roboto";
+        color: #FFFFFF;
+    }
+`
